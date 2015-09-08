620ae8b81cbb2efc6f5c58f4058b6d195002095c
Alan R. Soares <alanrsoars@gmail.com>
2015-09-09 00:08:34 +1200

    simplify neighbours logic

--- src/lib/game.js
+++ src/lib/game.js
@@ -1,16 +1,23 @@
 import compact from 'prelude-es6/lib/List/compact';
+import curry from 'prelude-es6/lib/Func/curry';
+import memoize from 'prelude-es6/lib/Func/memoize';
+
+const valueOrDefault = (val, def) => typeof val === 'undefined' ? def : val;
+const validIndex = curry((size, idx) => idx < 0 ? size : idx > size ? 0 : idx);
 
 export function getNeighbours(grid, { y, x }) {
-  const prev = grid[y - 1] || [];
-  const next = grid[y + 1] || [];
+  const v = memoize(validIndex(grid.length - 1));
+  const [prev, next] = [v(y - 1), v(y + 1)].map(id => grid[id]);
+  const ids = [v(x - 1), x, v(x + 1)];
+
   return [
-    prev[x - 1],    prev[x],   prev[x + 1],
-    grid[y][x - 1], grid[y][x + 1],
-    next[x - 1],    next[x],   next[x + 1]
+    ...ids.map((id) => prev[id]),
+    ...ids.map((id) => next[id]),
+    ...[v(x - 1), v(x + 1)].map((id) => grid[y][id])
   ];
 }
 
-export function getAliveNeighbours(grid, coords) {
+export function getAliveNeighbours(coords, grid) {
   const neighbours = getNeighbours(grid, coords);
   return compact(neighbours).length;
 }
@@ -23,7 +30,7 @@ export function isGoingToLive(isAlive, neighbours) {
 
 export function next(grid) {
   return grid.map((row, y) => row.map((column, x) =>
-    +isGoingToLive(column, getAliveNeighbours(grid, { x, y }))));
+    +isGoingToLive(column, getAliveNeighbours({ x, y }, grid))));
 }
 
 export function toggle({ y, x }, current, grid) {
