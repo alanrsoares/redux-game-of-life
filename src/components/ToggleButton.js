4798582e913a8ee55065c070d3163a23710f38df
Alan R. Soares <alanrsoars@gmail.com>
2016-02-16 10:43:56 +1300

    ToggleButton -> Pure functional component

--- src/components/ToggleButton.js
+++ src/components/ToggleButton.js
@@ -1,35 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 import cn from 'classnames'
 
 const NO_OP = () => null
 
-export default class ToggleButton extends Component {
-  render () {
-    const iconClasses = cn('fa', 'fa-lg', {
-      'active': this.props.on,
-      [this.props.onClass]: this.props.on,
-      [this.props.offClass]: !this.props.on
-    })
+const iconClasses = (on, onClass, offClass) =>
+  cn('fa', 'fa-lg', {
+    'active': on,
+    [onClass]: on,
+    [offClass]: !on
+  })
 
-    const buttonClasses = cn('btn', 'btn-default', {
-      'active': this.props.on
-    })
+const buttonClasses = (on) =>
+  cn('btn', 'btn-default', {
+    'active': on
+  })
 
-    return (
-      <label htmlFor='autoplay' className={buttonClasses}>
-        <i className={iconClasses}></i>
-        <input
-          id='autoplay'
-          style={{ display: 'none' }}
-          type='checkbox'
-          onChange={this.props.onClick}/> {this.props.label}
-      </label>
-    )
-  }
-}
-
-ToggleButton.defaultProps = {
-  onClass: 'fa-pause',
-  offClass: 'fa-play',
-  onClick: NO_OP
-}
+export default ({
+  on,
+  label,
+  onClass = 'fa-pause',
+  offClass = 'fa-play',
+  onClick = NO_OP
+}) =>
+  <label htmlFor='autoplay' className={buttonClasses(on)}>
+    <i className={iconClasses(on, onClass, offClass)}></i>
+    <input
+      id='autoplay'
+      style={{ display: 'none' }}
+      type='checkbox'
+      onChange={onClick}/> {label}
+  </label>
