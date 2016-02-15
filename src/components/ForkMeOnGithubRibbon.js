8e5408896185d05ae90e1e221a558d1e027e6b4a
Alan R. Soares <alanrsoars@gmail.com>
2016-02-16 10:46:21 +1300

    ForkMeOnGithubRibbon -> Pure functional component

--- src/components/ForkMeOnGithubRibbon.js
+++ src/components/ForkMeOnGithubRibbon.js
@@ -2,25 +2,14 @@ import React from 'react'
 
 const PROJECT_URL = 'https://github.com/alanrsoares/redux-game-of-life'
 
-export default class ForkMeOnGithubRibbon extends React.Component {
-  render () {
-    return (
-      <div>
-        <div className='github-fork-ribbon-wrapper right-bottom visible-xs'>
-          {this.renderRibbon()}
-        </div>
-        <div className='github-fork-ribbon-wrapper right hidden-xs'>
-          {this.renderRibbon()}
-        </div>
-      </div>
-    )
-  }
-
-  renderRibbon () {
-    return (
+export default () =>
+  <div>
+    <div className='github-fork-ribbon-wrapper right-bottom visible-xs'>
+      {this.renderRibbon()}
+    </div>
+    <div className='github-fork-ribbon-wrapper right hidden-xs'>
       <div className='github-fork-ribbon'>
         <a href={PROJECT_URL}>Fork me on Github</a>
       </div>
-    )
-  }
-}
+    </div>
+  </div>
