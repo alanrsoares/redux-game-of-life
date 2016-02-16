76a08caff6813d0bb1b5da2a1314919ece0ecb33
Alan R. Soares <alanrsoars@gmail.com>
2016-02-16 16:28:04 +1300

    Fix Ribbon component

--- src/components/ForkMeOnGithubRibbon.js
+++ src/components/ForkMeOnGithubRibbon.js
@@ -2,16 +2,17 @@ import React from 'react'
 
 const PROJECT_URL = 'https://github.com/alanrsoares/redux-game-of-life'
 
+const ribbon =
+  <div className='github-fork-ribbon'>
+    <a href={PROJECT_URL}>Fork me on Github</a>
+  </div>
+
 export default () =>
   <div>
     <div className='github-fork-ribbon-wrapper right-bottom visible-xs'>
-      <div className='github-fork-ribbon'>
-        <a href={PROJECT_URL}>Fork me on Github</a>
-      </div>
+      {ribbon}
     </div>
     <div className='github-fork-ribbon-wrapper right hidden-xs'>
-      <div className='github-fork-ribbon'>
-        <a href={PROJECT_URL}>Fork me on Github</a>
-      </div>
+      {ribbon}
     </div>
   </div>
